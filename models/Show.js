9ce7669122a5bf912f0aaed6d39b3528c2ebf60d
TobyWang1 <fufummumm@gmail.com>
2024-10-17 17:09:25 +0100

    Refactor Show and User models to extend Model class and use init method

--- models/Show.js
+++ models/Show.js
@@ -1,13 +1,21 @@
 // import our db, Model, DataTypes
-const { db, DataTypes } = require('../db/connection')
+const { db, DataTypes, Model } = require('../db/connection')
 
 // Creating a User child class from the Model parent class
-const Show = db.define('shows', {
-  title: DataTypes.STRING,
-  genre: DataTypes.STRING,
-  rating: DataTypes.INTEGER,
-  available: DataTypes.BOOLEAN
-})
+class Show extends Model {}
+
+Show.init(
+  {
+    title: DataTypes.STRING,
+    genre: DataTypes.STRING,
+    rating: DataTypes.INTEGER,
+    available: DataTypes.BOOLEAN
+  },
+  {
+    sequelize: db,
+    modelName: 'Show'
+  }
+)
 
 // exports
 module.exports = Show
